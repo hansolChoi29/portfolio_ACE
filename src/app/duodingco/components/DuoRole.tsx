87076cbc58bae18044027234580b914dc764cc56
hansolChoi29 <ggksthf29@gmail.com>
2025-05-30 18:50:23 +0900

    feat: motion 추가

--- src/app/duodingco/components/DuoRole.tsx
+++ src/app/duodingco/components/DuoRole.tsx
@@ -2,25 +2,49 @@
 import { motion } from "framer-motion";
 
 export default function DuoRole() {
+  const blockVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <motion.div>
-      <h1 className="my-24 font-bold sm:text-3xl text-xl w-full flex justify-center">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ staggerChildren: 0.2 }}
+      className="px-4"
+      variants={{
+        hidden: {},
+        visible: { transition: { staggerChildren: 0.2 } },
+      }}
+    >
+      <motion.h1
+        variants={blockVariants}
+        className="my-24 font-bold sm:text-3xl text-xl w-full flex justify-center"
+      >
         주요 역할
-      </h1>
-      <div className="border-b border-black py-4">
+      </motion.h1>
+      <motion.div
+        variants={blockVariants}
+        className="border-b border-black py-4"
+      >
         <p className="font-bold sm:text-xl text-base">단어 학습 기능 제공</p>
         <p className="sm:text-base text-sm">
           사용자가 단어를 학습할 수 있는 기능을 카드 형식으로 제공
         </p>
-      </div>
-      <div className="border-b border-black py-4">
+      </motion.div>
+      <motion.div
+        variants={blockVariants}
+        className="border-b border-black py-4"
+      >
         <p className="font-bold sm:text-xl text-base">북마크 관리</p>
         <p className="sm:text-base text-sm">
           사용자가 게시글을 북마크하여 나중에 다시 학습할 수 있도록 하고, 북마크
           상태를 실시간으로 업데이트
         </p>
-      </div>
-      <div className="py-4">
+      </motion.div>
+      <motion.div variants={blockVariants} className="py-4">
         <p className="font-bold sm:text-xl text-base">
           뜨끈하게 올라온 학습리스트
         </p>
@@ -28,7 +52,7 @@ export default function DuoRole() {
           Supabase의 시간순 정렬 로직을 활용해 최신 학습 내역을 한눈에 확인할 수
           있도록 구현한 페이지입니다.
         </p>
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
