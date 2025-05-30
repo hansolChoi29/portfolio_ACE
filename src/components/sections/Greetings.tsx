778092cf7b0d0567931ca2e44c3b080ce0d5bf3b
hansolChoi29 <ggksthf29@gmail.com>
2025-05-31 08:52:59 +0900

    style: story 마무리됨

--- src/components/sections/Greetings.tsx
+++ src/components/sections/Greetings.tsx
@@ -18,7 +18,12 @@ export default function Greetings() {
             <p>
               언젠간 관련회사에 들어가게 된다면, 적어도 이보단 잘 만들겠다싶어서
             </p>
-            <p>프론트엔드 개발자의 길을 꿈꾸게 되었습니다.</p>
+            <p>
+              프론트엔드 개발자로서{" "}
+              <strong>더 직관적인 인터페이스를 구현하고,</strong>
+              <br /> <strong>사용자 경험을 기술로 실현하는 개발자</strong>가
+              되고 싶다는 꿈이 생겼습니다.
+            </p>
           </div>
           <Image
             src="/images/home/emr.png"
@@ -53,6 +58,33 @@ export default function Greetings() {
             </p>
             <p>포기하고 싶었던 적도 있습니다.</p>
           </div>
+        </div>
+        <div className="flex items-center justify-between border-b border-t border-r rounded-l">
+          <div className="flex flex-col p-1 pr-10 gap-2">
+            <p>그럼에도 놓을 수 없는 이유는, 매순간</p>
+            <p>힘든 순간을 함께 이겨낸 동기들이 있고</p>
+            <p>
+              도움이 필요하면 마다하지않고 최선을 다해주신 튜터님이 계시기
+              때문에
+            </p>
+            <p>
+              무엇보다 개발하다 막히면 이제는 디버깅을 통해 데이터 흐름을
+              추적하고,
+            </p>
+            <p>문제를 해결할 때 느끼는 짜릿함이 가장 큰 원동력이 되었습니다.</p>
+
+            <p>
+              앞으로도 새로운 기술과 문제를 마주하더라도,
+              <strong>기회로 삼아 배우고 성장하는 개발자</strong>가 되겠습니다.
+            </p>
+          </div>
+          <Image
+            src="/images/home/winnersol.png"
+            alt="winnersol"
+            width={300}
+            height={100}
+            className="rounded"
+          />
         </div>
       </div>
     </div>
