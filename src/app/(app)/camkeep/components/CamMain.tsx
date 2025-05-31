a7a7686db1eff01d9499859de0469e56399ca7a2
hansolChoi29 <ggksthf29@gmail.com>
2025-05-31 10:05:42 +0900

    style: main image 뼈대만 잡음

--- src/app/(app)/camkeep/components/CamMain.tsx
+++ src/app/(app)/camkeep/components/CamMain.tsx
@@ -1,3 +1,39 @@
+"use client";
+import Image from "next/image";
+
 export default function CamMain() {
-  return <>newbie&main&checkList</>;
+  return (
+    <section>
+      <article>
+        <div>main</div>
+        <Image
+          src="/images/camkeep/keepmain.png"
+          alt="main"
+          width={200}
+          height={200}
+          priority
+        />
+      </article>
+      <article>
+        <div>newbie</div>{" "}
+        <Image
+          src="/images/camkeep/keepnewbie.png"
+          alt="main"
+          width={200}
+          height={200}
+          priority
+        />
+      </article>
+      <article>
+        <div>checkList</div>{" "}
+        <Image
+          src="/images/camkeep/keepcheck.png"
+          alt="main"
+          width={200}
+          height={200}
+          priority
+        />
+      </article>
+    </section>
+  );
 }
